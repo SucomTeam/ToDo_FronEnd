7362c106cf7e0d958e3c644101c8df32b31d90ce
JJS <arare30030@naver.com>
2023-07-27 19:54:02 +0900

    ToDo Save 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,38 @@
 <template>
-  <HelloWorld />
+  <v-responsive
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-text-field
+      v-model="todo"
+      clearable
+      hide-details="auto"
+      label="Last name"
+      @keyup.enter="saveToDo()"
+    ></v-text-field>
+  </v-responsive>
 </template>
 
-<script setup>
+<script>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from 'axios'
+
+  export default {
+    name : "Home",
+    data: () => ({
+      todo: '',
+    }),
+    methods: {
+      saveToDo() {
+        axios.post("/api/v1/todo/save", {
+          title : this.todo,
+        }).then((res) => {
+          console.log(res);
+        }).catch((err) => {
+          console.log(err);
+        })
+        console.log(this.todo);
+      }
+    },
+  }
 </script>
